8bce145432e02df1ee5d0648dd8ae798494d6c98
Eugenio Sales <eugenio.sales@outlook.com>
2021-09-06 19:48:37 -0300

    Update menu

--- src/menu/menu.c
+++ src/menu/menu.c
@@ -1,4 +1,4 @@
-#include "menu_controle.c"
+#include "menu.h"
 #include <stdio.h>
 
 int control_option;
@@ -8,7 +8,7 @@ float hysteresis;
 double KP, KI, KD;
 
 void menu(control_vars *params) {
-    printf("==== Bem vindo ao controle de temperatura ===="\n\n);
+    printf("==== Bem vindo ao controle de temperatura ====\n\n");
     printf("Ver se vai: %d\n", params->signal_key);
     control_menu();
     temp_reference_menu();
@@ -21,14 +21,15 @@ void control_menu() {
     printf("2) PID\n");
     printf("\nSelecione a estrategia de controle: ");
     
-    scanf("%d", &option_control);
+    scanf("%d", &control_option);
 
-    switch(option_control) {
+    switch(control_option) {
         case 1:
             printf("\nOn-off selecionado.\n");
             printf("Digite a histerese:");
             scanf("%f", &hysteresis);
             printf("\n");
+            break;
         case 2:
             printf("\nPID selecionado.\n\n");
             printf("Digite o KP:");
@@ -40,8 +41,9 @@ void control_menu() {
             printf("Digite o KD:");
             scanf("%f", &KD);
             printf("\n");
-        case 1:
-            printf("\nOpção inválida. Escolha novamente.\n\n")
+            break;
+        default:
+            printf("\nOpção inválida. Escolha novamente.\n\n");
             control_menu();
     }
 }
@@ -51,16 +53,18 @@ void temp_reference_menu() {
     printf("2) Via teclado\n");
     printf("\nSelecione a forma de entrada da temperatura de referência: ");
     
-    scanf("%d", &temp_reference_optionl);
+    scanf("%d", &temp_reference_option);
 
-    switch(option_control) {
+    switch(temp_reference_option) {
         case 1:
             printf("\nPotenciomtro selecionado.\n\n");
+            break;
         case 2:
             printf("\nDigite a Temp. de referencia:");
             scanf("%f", &temp_reference);
             printf("\n");
-        case 1:
+            break;
+        default:
             printf("\nOpção inválida. Escolha novamente.\n\n");
             temp_reference_menu();
     }
--- src/menu/menu.h
+++ src/menu/menu.h
@@ -1,6 +1,10 @@
 #ifndef CONTROL_MENU_H_
 #define CONTROL_MENU_H_
 
+#include "../uart_modbus/uart_modbus.h"
+
 void menu(control_vars *params);
+void control_menu();
+void temp_reference_menu();
 
 #endif /* CONTROL_MENU_H_ */
