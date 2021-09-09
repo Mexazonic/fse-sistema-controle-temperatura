38b557054af4b7ffea87a2d5c3cf184d26425fc6
Eugenio Sales <eugenio.sales@outlook.com>
2021-09-08 21:14:55 -0300

    Update makefile

--- src/on_off/on_off.c
+++ src/on_off/on_off.c
@@ -3,8 +3,8 @@
 double hysteresis;
 double temp_ref = 0.0;
 double temp_output = 0.0;
-int sinal_de_controle_MAX = 100.0;
-int sinal_de_controle_MIN = -100.0;
+int sinal_MAX = 100.0;
+int sinal_MIN = -100.0;
 
 void on_off_setup(float hysteresis_){
     hysteresis = hysteresis_;
@@ -19,9 +19,9 @@ double on_off_control(float temp_output){
 	double range = hysteresis / 2;
 	
 	if(temp_output < temp_ref - range) {
-		return sinal_de_controle_MAX;
+		return sinal_MAX;
 	} else if(temp_output > temp_ref + range) {
-		return sinal_de_controle_MIN;
+		return sinal_MIN;
 	} else {
 		return 0;
 	}
